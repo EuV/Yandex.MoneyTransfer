a3743007ee6e2bd880926522627ecd346fbf3daf
Vasilev Evgenij <vasil.ev.genij@gmail.com>
2015-09-25 15:04:53 +0400

    Refactor PIN Activity

--- app/src/main/java/my/yandex/money/transfer/PINActivity.java
+++ app/src/main/java/my/yandex/money/transfer/PINActivity.java
@@ -1,12 +1,18 @@
 package my.yandex.money.transfer;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import my.yandex.money.transfer.utils.Crypto;
 import my.yandex.money.transfer.utils.Notifications;
 import my.yandex.money.transfer.utils.Preferences;
+
+import static my.yandex.money.transfer.PINActivity.State.CONFIRM_PIN;
+import static my.yandex.money.transfer.PINActivity.State.CREATE_PIN;
+import static my.yandex.money.transfer.PINActivity.State.PROVIDE_PIN;
+
 
 public class PINActivity extends LogActivity {
     private static final String TAG = PINActivity.class.getName();
@@ -40,15 +46,13 @@ public class PINActivity extends LogActivity {
 
         plainToken = getIntent().getStringExtra(ACCESS_TOKEN_PLAIN);
         if (plainToken != null) {
-            state = State.CREATE_PIN;
-            label.setText(R.string.create_pin);
+            changeState(CREATE_PIN);
             return;
         }
 
         encryptedToken = getIntent().getStringExtra(ACCESS_TOKEN_ENCRYPTED);
         if (encryptedToken != null) {
-            state = State.PROVIDE_PIN;
-            label.setText(R.string.enter_pin);
+            changeState(PROVIDE_PIN);
             return;
         }
 
@@ -68,7 +72,7 @@ public class PINActivity extends LogActivity {
 
 
     @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
         encryptedToken = savedInstanceState.getString(ACCESS_TOKEN_ENCRYPTED);
         plainToken = savedInstanceState.getString(ACCESS_TOKEN_PLAIN);
@@ -79,11 +83,10 @@ public class PINActivity extends LogActivity {
 
 
     private class OkOnClickListener implements View.OnClickListener {
-        final EditText editor = (EditText) findViewById(R.id.pin_editor);
 
         @Override
         public void onClick(View okButton) {
-            String userPin = editor.getText().toString();
+            String userPin = ((EditText) findViewById(R.id.pin_editor)).getText().toString();
             if (userPin.length() < MIN_PIN_LENGTH) {
                 Notifications.showToUser(R.string.pin_min_length, MIN_PIN_LENGTH);
                 return;
@@ -96,9 +99,7 @@ public class PINActivity extends LogActivity {
             switch (state) {
                 case CREATE_PIN:
                     pin = userPin;
-                    label.setText(R.string.repeat_pin);
-                    editor.setText("");
-                    state = State.CONFIRM_PIN;
+                    changeState(CONFIRM_PIN);
                     break;
 
                 case CONFIRM_PIN:
@@ -110,9 +111,7 @@ public class PINActivity extends LogActivity {
                             Notifications.showToUser(R.string.can_not_log_in);
                         }
                     } else {
-                        label.setText(R.string.create_pin);
-                        editor.setText("");
-                        state = State.CREATE_PIN;
+                        changeState(CREATE_PIN);
                         Notifications.showToUser(R.string.pins_do_not_match);
                     }
                     break;
@@ -122,7 +121,7 @@ public class PINActivity extends LogActivity {
                     if (decryptAndCheck(encryptedToken, userPin)) {
                         goToPaymentActivity();
                     } else {
-                        editor.setText("");
+                        changeState(PROVIDE_PIN);
                         Notifications.showToUser(R.string.wrong_pin);
                         // TODO: logout
                     }
@@ -184,7 +183,27 @@ public class PINActivity extends LogActivity {
     }
 
 
-    private enum State {
+    private void changeState(State state) {
+        this.state = state;
+        ((EditText) findViewById(R.id.pin_editor)).setText("");
+
+        switch (state) {
+            case CREATE_PIN:
+                label.setText(R.string.create_pin);
+                break;
+
+            case CONFIRM_PIN:
+                label.setText(R.string.repeat_pin);
+                break;
+
+            case PROVIDE_PIN:
+                label.setText(R.string.enter_pin);
+                break;
+        }
+    }
+
+
+    enum State {
         CREATE_PIN,
         CONFIRM_PIN,
         PROVIDE_PIN
