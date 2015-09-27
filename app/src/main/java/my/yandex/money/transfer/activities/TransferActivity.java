f9c9961c21acfe46f5b718c08e2d6a2f610b335b
Vasilev Evgenij <vasil.ev.genij@gmail.com>
2015-09-28 03:38:29 +0400

    Add TransferActivity's layout

--- app/src/main/java/my/yandex/money/transfer/activities/TransferActivity.java
+++ app/src/main/java/my/yandex/money/transfer/activities/TransferActivity.java
@@ -1,7 +1,14 @@
 package my.yandex.money.transfer.activities;
 
+import android.os.Bundle;
+import my.yandex.money.transfer.R;
 import my.yandex.money.transfer.activities.hierarchy.SecurityActivity;
 
 public class TransferActivity extends SecurityActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transfer);
+    }
 }
