6e169a7edc7bb4771214dd81280560aeafbed573
Vasilev Evgenij <vasil.ev.genij@gmail.com>
2015-09-29 21:37:40 +0400

    Set up operation view

--- app/src/main/java/my/yandex/money/transfer/HistoryAdapter.java
+++ app/src/main/java/my/yandex/money/transfer/HistoryAdapter.java
@@ -4,12 +4,15 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import com.yandex.money.api.model.Operation;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+
+import static com.yandex.money.api.model.Operation.Direction.INCOMING;
 
 public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.OperationViewHolder> {
     private List<Operation> operations = new ArrayList<>();
@@ -35,8 +38,14 @@ public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.Operatio
 
 
     @Override
-    public void onBindViewHolder(OperationViewHolder operationViewHolder, int i) {
-        operationViewHolder.name.setText(operations.get(i).title);
+    public void onBindViewHolder(OperationViewHolder holder, int i) {
+        holder.title.setText(operations.get(i).title);
+        holder.amount.setText(operations.get(i).amount.toString());
+        if (operations.get(i).direction == INCOMING) {
+            holder.direction.setImageResource(R.drawable.ic_keyboard_tab_black_24dp_left);
+        } else {
+            holder.direction.setImageResource(R.drawable.ic_keyboard_tab_black_24dp);
+        }
     }
 
 
@@ -47,11 +56,15 @@ public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.Operatio
 
 
     class OperationViewHolder extends RecyclerView.ViewHolder {
-        TextView name;
+        TextView title;
+        ImageView direction;
+        TextView amount;
 
         public OperationViewHolder(View operation) {
             super(operation);
-            name = (TextView) operation.findViewById(R.id.name);
+            title = (TextView) operation.findViewById(R.id.title);
+            amount = (TextView) operation.findViewById(R.id.amount);
+            direction = (ImageView) operation.findViewById(R.id.direction);
         }
     }
 }
